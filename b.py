61a1d31681f78950a81c6e30581ed1be4a9c815f
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2026-02-17 18:18:38 +0300

    Create b.py

---
+++ b.py
@@ -0,0 +1,11 @@
+if os.name == "nt":
+  socket_protocol = socket.IPPROTO_IP
+else:
+  socket_protocol = socket.IPPROTO_ICMP
+
+sniffer = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket_protocol)
+sniffer.bind((host, 0))
+sniffer.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+if os.name == "nt":
+  sniffer.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
